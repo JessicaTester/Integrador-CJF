3e03387d47d25295d73260b621bdc90022452e37
Claudia Fernandez <claudiajessicatelevisor@gmail.com>
2024-05-22 15:12:43 -0300

    Carrusel

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Integrador-CJF
+## Proyecto Integrador
+- Carrusel
+- Formulario contacto
+### Usando
+  ![HTML5](https://img.shields.io/badge/-HTML5-333333?style=flat&logo=HTML5)
+
+  ![CSS](https://img.shields.io/badge/-CSS-333333?style=flat&logo=CSS3&logoColor=1572B6)
+  
+  ![JavaScript](https://img.shields.io/badge/-JavaScript-333333?style=flat&logo=javascript)
--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+        const btAnterior = document.querySelector('.btnAnterior');
+        const btSiguiente = document.querySelector('.btnSiguiente');
+        const carrContenido = document.querySelector('.carruContenido');
+        const carrImgs = document.querySelector('.carruImgs');
+        const imgs = document.querySelectorAll('.carruImgs img');
+        let contador = 0;
 
+        const tam = carrContenido.clientWidth;
+        console.log(tam);
+        btSiguiente.addEventListener('click', () => {
+            contador++;
+            if (contador === imgs.length) {
+                contador = 0;
+            }
+            carrImgs.style.transform = `translateX(-${tam * contador}px)`;
+        });
+});
